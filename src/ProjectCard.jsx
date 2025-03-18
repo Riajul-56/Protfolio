235330a64a470417ce3596338b0d7f1ac3f2b8dd
Riajul-56 <riajulhasansiam@gmail.com>
2025-03-18 21:30:24 +0600

    refactor ProjectCard component to accept props for title and description, and update Projects section to utilize new props

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Flex from "./compoents/Flex";
 import project_1 from "./assets/project_1.png";
-import project_2 from "./assets/project_2.png";
 
-
-const ProjectCard = () => {
+const ProjectCard = ({text,h3}) => {
   return (
-    <Flex>
+    <Flex className="gap-12 items-center mt-20">
+
+
       <Flex className="bg-[#1A1A1A] w-[600px] h-[600px] items-center justify-center rounded-lg">
         <img
           src={project_1}
           className="w-[500px] h-[347px] rounded-xl border border-primary_bg"
         />
       </Flex>
+
+      <div>
+        <h3 className="font-secondary_font font-medium text-heading_text text-[32px] leading-[140%] max-w-[576px]">
+          {h3}
+        </h3>
+        <p className="font-secondary_font font-normal text-primary_text text-[18px] leading-[150%] max-w-[576px] mt-4 mb-8">
+          {text}
+        </p>
+      </div>
+
+
     </Flex>
   );
 };
--- src/section/Projects.jsx
+++ src/section/Projects.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Container from "../compoents/Container";
 import ProjectCard from "../ProjectCard";
-import Flex from "../compoents/Flex";
 
 const Projects = () => {
   return (
@@ -17,21 +16,12 @@ const Projects = () => {
           </p>
         </div>
 
-        <Flex className="gap-12 items-center">
-          <ProjectCard />
-          <div>
-            <h3 className="font-secondary_font font-medium text-heading_text text-[32px] leading-[140%] max-w-[576px]">
-              Promotional landing page for our favorite show
-            </h3>
-            <p className="font-secondary_font font-normal text-primary_text text-[18px] leading-[150%] max-w-[576px] mt-4 mb-8">
-              Teamed up with a designer to breathe life into a promotional
-              webpage for our beloved show, Adventure Time. Delivered a fully
-              responsive design with dynamic content capabilities, seamlessly
-              integrating a newsletter feature to keep fans updated with the
-              latest adventures.
-            </p>
-          </div>
-        </Flex>
+        <ProjectCard
+          h3={"Promotional landing page for our favorite show"}
+          text={
+            "Teamed up with a designer to breathe life into a promotional webpage for our beloved show, Adventure Time. Delivered a fully responsive design with dynamic content capabilities, seamlessly integrating a newsletter feature to keep fans updated with the latest adventures."
+          }
+        />
       </Container>
     </section>
   );
